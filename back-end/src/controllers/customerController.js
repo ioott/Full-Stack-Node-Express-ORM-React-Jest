6a0511593e461290696f5bd51584bbc1ac8e781a
miguelinux <miguellimaoliver97@gmail.com>
2022-11-29 18:37:50 -0300

    Controllers, Register, Login and Customer

---
+++ back-end/src/controllers/customerController.js
@@ -0,0 +1,20 @@
+const { getAllProducts, addNewSale, getAllSellers } = require('../services/customerService');
+
+const getAll = async (req, res) => {
+  const data = await getAllProducts();
+  return res.status(200).json(data);   
+};
+
+const addSale = async (req, res) => {
+  // const { products, userId, sellerId, totalPrice, deliveryAddress,
+  //    deliveryNumber, saleDate, status } = req.body;
+  const data = await addNewSale(req.body);
+  return res.status(201).json(data);
+};
+
+const getSeller = async (req, res) => {
+  const data = await getAllSellers();
+  return res.status(201).json(data);
+};
+
+module.exports = { getAll, addSale, getSeller };
---
+++ back-end/src/controllers/registerController.js
@@ -0,0 +1,18 @@
+const md5 = require('md5');
+const { createToken } = require('../services/jwtService');
+const { validateCreateBody, checkUser, createUser } = require('../services/registerService');
+const { validateBody } = require('../services/loginService');
+
+const create = async (req, res) => {
+  const { name, password: bodyPassword, email, role } = req.body;
+  validateBody({ email, bodyPassword });
+  validateCreateBody({ name, role });
+  await checkUser(name, email);
+  const encryptedPass = md5(bodyPassword);
+  const newUser = await createUser({ name, email, password: encryptedPass, role });
+  const { password, id, ...info } = newUser;
+  const token = await createToken(info);
+  return res.status(201).json({ ...info, token });
+};
+
+module.exports = { create };
