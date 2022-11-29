9803c025a2323d9467c5b2fd33a74d7fecb7ec1f
miguelinux <miguellimaoliver97@gmail.com>
2022-11-29 18:37:17 -0300

    Apis

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
+require('express-async-errors');
+const cors = require('cors');
+const errorHandlerMiddleware = require('../middlewares/errorHandlerMiddleware');
+const loginRoute = require('../routes/loginRoute');
+const registerRoute = require('../routes/registerRoute');
+const customerRoute = require('../routes/customerRoute');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/login', loginRoute);
+
+app.use('/register', registerRoute);
+
+app.use('/customer', customerRoute);
+
+app.use('/images', express.static('public'));
+
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
