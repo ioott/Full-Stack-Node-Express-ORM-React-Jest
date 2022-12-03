453294e39f010b1c6ba7dc95d76cf2979b1bbdff
Raphael Cirelli <raphael.cirelli@protonmail.com>
2022-12-03 19:08:42 -0300

    fix: arruma array de dependencias useLocalStorage

--- front-end/src/hooks/useLocalStorage.jsx
+++ front-end/src/hooks/useLocalStorage.jsx
@@ -1,27 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function useLocalStorage(key, initialValue) {
   const [savedValue, setSavedValue] = useState('');
   const [newValue, setNewValue] = useState('');
+  const keyRef = useRef(key);
+  const initialValueRef = useRef(initialValue);
 
   useEffect(() => {
-    const savedItem = localStorage.getItem(key);
+    const savedItem = localStorage.getItem(keyRef.current);
     if (savedItem === null) {
-      localStorage.setItem(key, JSON.stringify(initialValue));
-      setSavedValue(initialValue);
+      localStorage.setItem(keyRef.current, JSON.stringify(initialValueRef.current));
+      setSavedValue(initialValueRef.current);
     } else {
       setSavedValue(JSON.parse(savedItem));
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
-    const savedItem = window.localStorage.getItem(key);
+    const savedItem = window.localStorage.getItem(keyRef.current);
     if (newValue !== savedItem && (newValue !== '' && newValue !== null)) {
-      localStorage.setItem(key, JSON.stringify(newValue));
+      localStorage.setItem(keyRef.current, JSON.stringify(newValue));
       setSavedValue(newValue);
     }
-  }, [newValue, key]);
+  }, [newValue, keyRef]);
 
   return [savedValue, setNewValue];
 }
